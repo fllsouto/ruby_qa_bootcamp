71471251f5bd3da371bf4ada6b691f78b7fb56db
Fellipe Souto <fllsouto@gmail.com>
2016-09-19 15:08:21 -0300

    Nice print for test results

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ echo "Starting Deploy Script"
 echo "Version : `firefox -v`"
 echo "Display: $DISPLAY"
 echo "Process Xvfb: \n`ps aux | grep Xvfb`"
-bundle exec rspec spec
+bundle exec rspec spec -bc --format d
